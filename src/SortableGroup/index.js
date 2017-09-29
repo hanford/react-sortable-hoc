ccab0d43e3fc771cb403298ff2bc18dcaaa42098
hanford <jackhanford@gmail.com>
2017-09-29 14:10:19 -0700

    add prop-types

--- src/SortableGroup/index.js
+++ src/SortableGroup/index.js
@@ -1,4 +1,5 @@
-import {Component, PropTypes} from 'react';
+import {Component} from 'react';
+import PropTypes from 'prop-types'
 import debounce from 'lodash/debounce';
 import {
   closestNodeIndex,
